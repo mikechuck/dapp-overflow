c9a64b482e2c570d6b67d3de2b4027004d6f544b
Michael <mcjoy465@gmail.com>
2021-07-17 14:06:09 -0400

    style edits

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,47 +8,7 @@ class Main extends Component {
 		return (
 			<div className="container-fluid mt-5 main-container">
 				<main role="main" className="row">
-					<div className="column-left col-sm-3">
-						<div className="topics-container">
-							<div className="header">
-								<h3>
-									Topics
-									<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="stream" className="svg-inline--fa fa-stream fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M16 128h416c8.84 0 16-7.16 16-16V48c0-8.84-7.16-16-16-16H16C7.16 32 0 39.16 0 48v64c0 8.84 7.16 16 16 16zm480 80H80c-8.84 0-16 7.16-16 16v64c0 8.84 7.16 16 16 16h416c8.84 0 16-7.16 16-16v-64c0-8.84-7.16-16-16-16zm-64 176H16c-8.84 0-16 7.16-16 16v64c0 8.84 7.16 16 16 16h416c8.84 0 16-7.16 16-16v-64c0-8.84-7.16-16-16-16z"></path></svg>
-								</h3>
-							</div>
-							<div className="content">
-								<ul className="topics-list">
-									<li>Solidity</li>
-									<li>React</li>
-									<li>Ethereum</li>
-									<li>Dapps</li>
-									<li>DeFi</li>
-									<li>Solana</li>
-									<li>Othersd</li>
-								</ul>
-							</div>
-						</div>
-						<div className="trending-container">
-							<div className="header">
-								<h3>
-									Trending
-									<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chart-line" className="svg-inline--fa fa-chart-line fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M496 384H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16zM464 96H345.94c-21.38 0-32.09 25.85-16.97 40.97l32.4 32.4L288 242.75l-73.37-73.37c-12.5-12.5-32.76-12.5-45.25 0l-68.69 68.69c-6.25 6.25-6.25 16.38 0 22.63l22.62 22.62c6.25 6.25 16.38 6.25 22.63 0L192 237.25l73.37 73.37c12.5 12.5 32.76 12.5 45.25 0l96-96 32.4 32.4c15.12 15.12 40.97 4.41 40.97-16.97V112c.01-8.84-7.15-16-15.99-16z"></path></svg>
-								</h3>
-							</div>
-							<div className="content">
-								<ul className="trending-list">
-									<li>How do I write solidity?</li>
-									<li>What is blockchain?</li>
-									<li>Invalid DOM property `class`. Did you mean t...</li>
-									<li>ERROR: File not found: '../logo.png'</li>
-									<li>How do I deploy to the blockchain? I can't s...</li>
-									<li>Solana</li>
-									<li>Othersd</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div className="column-middle col-sm-7">
+					<div className="column-middle col-sm-6">
 						<h2>Ask a Question</h2>
 						<form onSubmit={(event) => {
 							event.preventDefault()
@@ -111,7 +71,6 @@ class Main extends Component {
 							)
 						})}
 					</div>
-					<div className="column-right col-sm-2"></div>
 				</main>
 			</div>
 		);
